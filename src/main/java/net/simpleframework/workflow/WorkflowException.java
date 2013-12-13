07ccc409b09bfc29a82754c05853210555dca0de
simpleframework <cknet@126.com>
2013-12-13 13:18:34 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/workflow/WorkflowException.java
+++ src/main/java/net/simpleframework/workflow/WorkflowException.java
@@ -1,6 +1,6 @@
 package net.simpleframework.workflow;
 
-import net.simpleframework.common.th.RuntimeExceptionEx;
+import net.simpleframework.ctx.ModuleException;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -8,18 +8,18 @@ import net.simpleframework.common.th.RuntimeExceptionEx;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public class WorkflowException extends RuntimeExceptionEx {
+public class WorkflowException extends ModuleException {
 	private static final long serialVersionUID = 7984366934401318860L;
 
 	public WorkflowException(final String msg, final Throwable cause) {
 		super(msg, cause);
 	}
 
-	public static RuntimeException of(final Throwable throwable) {
-		return _of(WorkflowException.class, null, throwable);
+	public static WorkflowException of(final Throwable throwable) {
+		return (WorkflowException) _of(WorkflowException.class, null, throwable);
 	}
 
-	public static RuntimeException of(final String msg) {
+	public static WorkflowException of(final String msg) {
 		return _of(WorkflowException.class, msg);
 	}
 }
